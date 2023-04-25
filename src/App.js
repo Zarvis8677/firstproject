e40f6bc792d4866afdff66258246ff0e847cdd40
Zarvi8677 <nishantnikhilm6@gmail.com>
2023-04-25 20:36:27 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import { useEffect, useState, React } from 'react';
+function App() {
+
+  const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        setInterval(() => {
+        setCount(count + 1);
+        }, 1000)
+    },);
+
+  return (
+    <div className="App">
+      <h1>The Count is: </h1>
+      <h2>{count}</h2>
+      </div>
+  )
+}
+      
+export default App;
